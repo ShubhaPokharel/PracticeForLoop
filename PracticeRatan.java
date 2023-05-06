317acf0bf692833c14ff738109ae224d94b1ede5
ꜱʜᴜʙʜᴀ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-05-06 12:30:56 -0400

    Add files via upload
    For loop practice: in example one, In this for loop Aruna and Sushil get printed first only once. Anu will get printed 10 times. Example two: In this for loop, Durga will get printed 10 times. The only difference is that the initialization part is typed out of the for loop. Example three: In this for loop hey will get printed 10 times.  Example Four: In this for loop, Pneumonoultramicroscopicsilicovolcanoconiosis will get printed infinity times.

---
+++ PracticeRatan.java
@@ -0,0 +1,33 @@
+class PracticeRatan{
+	public static void main(String[] args){
+
+		//Example 1
+		//In this for loop Aruna and Sushil get printed first only once. Anu will get printed 10 times
+    //  int i = 0;
+	// for(System.out.println("Aruna"),System.out.println("Sushil"); i<10;i++){
+	// System.out.println("Anu");
+	// }
+
+
+        //Example 2
+        //In this for loop, Durga will get printed 10 times. The only difference is that the initialization part is typed out of the for loop.
+		// int i = 0;
+		// for(;i<10;i++){
+		// 	System.out.print("durga");
+		// }
+
+
+         //Example 3
+		//In this for loop hey will get printed 10 times.
+		// for(int i = 0, j = 0;i<10;i++){
+		// 	System.out.println("hey");
+		// }
+
+		
+		//Example 4
+		//In this for loop, Pneumonoultramicroscopicsilicovolcanoconiosis will get printed infinity times
+		 for(int i = 0;;i++){
+		 	System.out.println("Pneumonoultramicroscopicsilicovolcanoconiosis");
+		 }
+	}
+}
